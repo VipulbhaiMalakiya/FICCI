af4359c54e18e2031ff8e3a55bc440ecd539610b
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-10 10:45:21 +0530

    update

--- src/app/Modules/Masters/configuration-list/service/configuration.service.ts
+++ src/app/Modules/Masters/configuration-list/service/configuration.service.ts
@@ -12,7 +12,7 @@ export class ConfigurationService {
 
   private apiUrl = `${environment.apiURL}Configuration_CRUD` ;
   private categoryListUrl = `${environment.apiURL}Drp_CategoryList` ;
-
+  private  retry:any =  retry(1); // Retry the request up to 2 times in case of failure
   constructor(private http: HttpClient) {}
 
   getCategoryList(): Observable<Category[]> {
@@ -21,19 +21,15 @@ export class ConfigurationService {
     );
   }
   getAll(): Observable<Configuration[]> {
-    return this.http.get<Configuration[]>(`${this.apiUrl}`).pipe(
-      retry(1),
-    );
+    return this.http.get<Configuration[]>(`${this.apiUrl}`).pipe(this.retry);
   }
 
   create(data: addUpdateConfiguration): Observable<addUpdateConfiguration> {
-    return this.http.post<addUpdateConfiguration>(`${this.apiUrl}`, data).pipe(
-      retry(1),
-    );
+    return this.http.post<addUpdateConfiguration>(`${this.apiUrl}`, data).pipe(this.retry);
   }
 
   delete(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}?C_ID=${id}`);
+    return this.http.delete<any>(`${this.apiUrl}?C_ID=${id}`).pipe(this.retry);
   }
 
 }
