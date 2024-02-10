fbac016c33a5662becf60bde0fdd59cdef340725
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-10 10:31:47 +0530

    update

--- src/app/Modules/Masters/configuration-list/pages/configuration-list/configuration-list.component.ts
+++ src/app/Modules/Masters/configuration-list/pages/configuration-list/configuration-list.component.ts
@@ -149,6 +149,9 @@ export class ConfigurationListComponent implements OnInit , OnDestroy  {
       .catch(() => {});
   }
 
+  trackById(index: number, item: any): number {
+    return item.id; // Assuming your item has a unique identifier property called 'id'
+  }
 
 
   onEdit(data: Configuration): void {
