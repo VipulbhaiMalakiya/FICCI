3665aa87a21de250331cad1cd9d571195aa9c0a6
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-11 09:44:00 +0530

    update

--- src/app/Modules/Masters/configuration-list/configuration-list.module.ts
+++ src/app/Modules/Masters/configuration-list/configuration-list.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { ConfigurationListRoutingModule } from './configuration-list-routing.module';
-import { ConfigurationListComponent } from './pages/configuration-list/configuration-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from '../../shared/shared.module';
 import { NgxPaginationModule } from 'ngx-pagination'; // Import ngx-pagination module
-import { SearchFilterPipe } from './pipe/searchFilter';
 import { NgSelectModule } from '@ng-select/ng-select';
-
+import {  ConfigurationListRoutingModule,ConfigurationListComponent,SharedModule,SearchFilterPipe} from './import/model';
 @NgModule({
   declarations: [
     ConfigurationListComponent,
---
+++ src/app/Modules/Masters/configuration-list/import/model.ts
@@ -0,0 +1,4 @@
+export { ConfigurationListRoutingModule } from '../configuration-list-routing.module';
+export { ConfigurationListComponent } from '../pages/configuration-list/configuration-list.component';
+export { SharedModule } from '../../../shared/shared.module';
+export { SearchFilterPipe } from '../pipe/searchFilter';
