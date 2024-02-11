3526ba22407c3e8aebcd742209ed4f2e2e00829b
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-11 07:57:11 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from "@angular/common/http"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
-import { ConfirmationDialogModalComponent } from './Modules/shared/components/confirmation-dialog-modal/confirmation-dialog-modal.component';
+import {NgModule,BrowserModule,HttpClientModule,AppComponent,FormsModule,ReactiveFormsModule,BrowserAnimationsModule,ToastrModule,AppRoutingModule} from './import/app'
 @NgModule({
   declarations: [
     AppComponent,
-    ConfirmationDialogModalComponent,
-
   ],
   imports: [
     BrowserModule,
