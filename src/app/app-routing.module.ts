9dac5a04023aeb5297cdf878f34343fbaeb94796
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-11 11:05:03 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SidebarLayoutComponent } from './layouts/sidebar-layout/sidebar-layout.component';
 
+
+const sidebarLayoutRoutes = [
+  {
+    path: '',
+    component: SidebarLayoutComponent,
+    children: [
+      { path: 'configuration-list', loadChildren: () => import('./Modules/Masters/configuration-list/configuration-list.module').then(m => m.ConfigurationListModule) },
+      { path: 'users', loadChildren: () => import('./Modules/Masters/users/users.module').then(m => m.UsersModule) }
+    ]
+  }
+];
+
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./Modules/dashboard/dashboard.module').then(m => m.DashboardModule)
   },
-  {
-    path:'master',
-    component:SidebarLayoutComponent
-  },
-  { path: 'masters/configuration-list', loadChildren: () => import('./Modules/Masters/configuration-list/configuration-list.module').then(m => m.ConfigurationListModule) },
-  { path: 'masters/users', loadChildren: () => import('./Modules/Masters/users/users.module').then(m => m.UsersModule) }];
+
+
+  { path: 'masters', children: sidebarLayoutRoutes },
+  { path: 'customer', children: sidebarLayoutRoutes },
+  { path: 'invoice', children: sidebarLayoutRoutes }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
