6c64ff1ce12ce15aafe8acfac994df6db3f1615c
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-12 14:59:39 +0530

    update

--- src/app/Modules/Masters/users/components/add/add.component.ts
+++ src/app/Modules/Masters/users/components/add/add.component.ts
@@ -168,8 +168,6 @@ export class AddComponent implements OnInit {
   // Method to Patch data edite time
   patchFormWithData(): void {
     this.publicVariable.userData = history.state.data;
-    console.log(this.publicVariable.userData);
-
     if (this.publicVariable.userData) {
       this.onEdit(this.publicVariable.userData);
     }
