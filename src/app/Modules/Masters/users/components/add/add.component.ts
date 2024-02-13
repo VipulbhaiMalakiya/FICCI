77c94ec935396713abc63a0af37b9170601f8d23
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-13 23:40:49 +0530

    update

--- src/app/Modules/Masters/users/components/add/add.component.ts
+++ src/app/Modules/Masters/users/components/add/add.component.ts
@@ -14,7 +14,6 @@ export class AddComponent implements OnInit {
     private modalService: NgbModal,
     private toastr: ToastrService,
     private API: UserService,
-    private cdr: ChangeDetectorRef,
     private router: Router,
     private route: ActivatedRoute
 
@@ -41,7 +40,6 @@ export class AddComponent implements OnInit {
       if (params && params.get('id')) {
         this.userId = +params.get('id')!;
         this.fetchUserData(this.userId);
-        this.cdr.markForCheck();
       }
     });
 
@@ -50,13 +48,34 @@ export class AddComponent implements OnInit {
   fetchUserData(userId: number): void {
     this.API.getUserById(userId).subscribe(data => {
       this.publicVariable.userData = data.data[0];
-
       if (this.publicVariable.userData) {
-        this.cdr.detectChanges();
         this.onEdit(this.publicVariable.userData);
-        this.publicVariable.isProcess = false;
       }
     });
+  }
+
+  getRoles(): void {
+    this.API.getRoles().subscribe({
+      next: (data: any) => {
+        this.publicVariable.roles = data.data.map((role: { roleName: string }) => ({
+          ...role,
+        }));
+      },
+      error: (error: any) => {
+        this.publicVariable.roles = DEFAULT_ROLE_LIST;
+      }
+    });
+  }
+
+  loadEmpoyeeList(): void {
+    this.publicVariable.Subscription.add(
+      this.API.getEmployee().subscribe({
+        next: (response: any) => {
+          this.publicVariable.employeeList = response.data;
+          this.publicVariable.isProcess = false;
+        }
+      })
+    );
   }
 
   onSelectEmployee() {
@@ -79,85 +98,68 @@ export class AddComponent implements OnInit {
     }
   }
 
-  onDelete(id: number) {
+  onDelete(id: number): void {
     const modalRef = this.modalService.open(ConfirmationDialogModalComponent, { size: "md", centered: true, backdrop: "static" });
-    var componentInstance = modalRef.componentInstance as ConfirmationDialogModalComponent;
+    const componentInstance = modalRef.componentInstance as ConfirmationDialogModalComponent;
     componentInstance.message = "Do you really want to delete these records? This process cannot be undone ?";
+
     modalRef.result.then((canDelete: boolean) => {
-
       if (canDelete) {
-        this.publicVariable.isProcess = true;
-        this.API.delete(id).subscribe({
-          next: (res: any) => {
-            this.toastr.success(res.message, 'Success');
-            this.router.navigate(['/masters/users']);
-            this.publicVariable.isProcess = false;
-          },
-          error: (error) => {
-            this.toastr.error(error.error.message, 'Error');
-
-          }
-        });
-
+        this.deleteRecord(id);
       }
-    }).catch(() => {
-    });
-
+    }).catch(() => { });
   }
 
-  onSubmit() {
+  deleteRecord(id: number): void {
+    this.publicVariable.isProcess = true;
+    this.API.delete(id).subscribe({
+      next: (res: any) => {
+        this.handleDeleteSuccess(res);
+      },
+      error: (error: any) => {
+        this.handleDeleteError(error);
+      }
+    });
+  }
 
+  handleDeleteSuccess(response: any): void {
+    this.toastr.success(response.message, 'Success');
+    this.router.navigate(['/masters/users']);
+    this.publicVariable.isProcess = false;
+  }
 
+  handleDeleteError(error: any): void {
+    this.toastr.error(error.error.message, 'Error');
+    this.publicVariable.isProcess = false;
+  }
+
+  onSubmit(): void {
     if (this.publicVariable.dataForm.valid) {
       const newData = this.publicVariable.dataForm.value;
       const isUpdate = !!newData.id;
       this.publicVariable.isProcess = true;
-      this.publicVariable.selectedEmployee = this.publicVariable.employeeList.find(employee => employee.imeM_EmpId == newData.empId || this.publicVariable.userData.imeM_EmpId);
+
+      const selectedEmployee = this.publicVariable.employeeList.find(employee => employee.imeM_EmpId === newData.empId) || this.publicVariable.userData;
+
       const newConfig: addUpdateEmployees = {
         isUpdate: isUpdate,
         imeM_ID: isUpdate ? newData.id : undefined,
         imeM_EmpId: newData.empId || this.publicVariable.userData.imeM_EmpId,
-        imeM_Username: this.publicVariable.selectedEmployee.imeM_Username,
-        imeM_Name: this.publicVariable.selectedEmployee.imeM_Name,
-        imeM_Email: this.publicVariable.selectedEmployee.imeM_Email,
+        imeM_Username: selectedEmployee.imeM_Username,
+        imeM_Name: selectedEmployee.imeM_Name,
+        imeM_Email: selectedEmployee.imeM_Email,
         roleId: newData.roleId,
         isActive: newData.isActive
       };
 
       const successMessage = isUpdate ? 'Data updated successfully.' : 'Data created successfully.';
       this.handleApiRequest(this.API.create(newConfig), successMessage, 'Error submitting data:');
-
     } else {
       this.markFormControlsAsTouched();
     }
-
   }
 
-  // Method to fetch roles
-
-  getRoles() {
-    this.API.getRoles().subscribe({
-      next: (data: any) => {
-        this.publicVariable.roles = data.data.map((role: { roleName: string }) => ({
-          ...role,
-        }));
-        this.publicVariable.isProcess = false;
-      },
-      error: (error: any) => {
-        this.publicVariable.isProcess = false;
-        this.publicVariable.roles = DEFAULT_ROLE_LIST;
-      }
-    });
-  }
-
-  getRoleIdByRoleName(roleName: string): number | undefined {
-    const role = this.publicVariable.roles.find(role => role.roleName === roleName);
-    return role ? role.role_id : undefined;
-  }
-
-
-
-  onEdit(data: any) {
+  onEdit(data: any): void {
     this.publicVariable.isEdit = true;
     const RoleId: any = this.getRoleIdByRoleName(data.roleName);
     this.publicVariable.dataForm.patchValue({
@@ -172,45 +174,18 @@ export class AddComponent implements OnInit {
     this.publicVariable.dataForm.controls["empId"].disable();
   }
 
-  // Method to fetch employess
-
-  loadEmpoyeeList(): void {
-    try {
-      this.publicVariable.Subscription.add(
-        this.API.getEmployee().subscribe({
-          next: (response: any) => {
-            this.publicVariable.employeeList = response.data
-          },
-          error: () => {
-
-          }
-        })
-      );
-    } catch (error) {
-      console.error('Error loading category list:', error);
-    }
+  getRoleIdByRoleName(roleName: string): number | undefined {
+    const role = this.publicVariable.roles.find(role => role.roleName === roleName);
+    return role ? role.role_id : undefined;
   }
 
-  //handleApiResponse
-  handleApiResponse(res: any, successMessage: string): void {
-    if (res.status === true) {
-      this.toastr.success(successMessage, 'Success');
-      this.publicVariable.userData = res.data;
-      this.publicVariable.dataForm.reset();
-    } else {
-      this.toastr.error(res.message, 'Error');
-    }
-  }
-
-  //handleApiRequest
   handleApiRequest(apiRequest: any, successMessage: string, errorMessagePrefix: string): void {
     try {
       this.publicVariable.Subscription.add(
         apiRequest.subscribe({
           next: (res: any) => {
-            this.publicVariable.userData = res.data
+            this.publicVariable.userData = res.data;
             this.publicVariable.isProcess = false;
-
             this.handleApiResponse(res, successMessage);
           },
           error: (error: any) => {
@@ -224,15 +199,24 @@ export class AddComponent implements OnInit {
       this.toastr.error('Error handling API request', 'Error');
     }
   }
-  //Filed Define to show error in sumit time
+
+  handleApiResponse(res: any, successMessage: string): void {
+    if (res.status === true) {
+      this.toastr.success(successMessage, 'Success');
+      this.publicVariable.userData = res.data;
+      this.publicVariable.dataForm.reset();
+    } else {
+      this.toastr.error(res.message, 'Error');
+    }
+  }
+
   markFormControlsAsTouched(): void {
     ['empId', 'username', 'name', 'email', 'roleId'].forEach(controlName => {
       this.publicVariable.dataForm.controls[controlName].markAsTouched();
     });
   }
 
-  //Error Function
-  shouldShowError(controlName: string, errorName: string) {
+  shouldShowError(controlName: string, errorName: string): boolean {
     return this.publicVariable.dataForm.controls[controlName].touched && this.publicVariable.dataForm.controls[controlName].hasError(errorName);
   }
 }
