a193be759f0a98c5b37ea2bbb74b84c73cda6cb3
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-15 09:44:44 +0530

    update

--- src/app/Modules/invoice/component/invoice-status/invoice-status.component.ts
+++ src/app/Modules/invoice/component/invoice-status/invoice-status.component.ts
@@ -22,15 +22,18 @@ export class InvoiceStatusComponent {
 
   onDownload() {
     // const exportData = this.publicVariable.userlist.map((x) => ({
-    //   "Cust. No.": x?.imeM_EmpId || '',
-    //   Name: x?.imeM_Name || '',
-    //   Address: x?.imeM_Username || '',
-    //   City: x?.imeM_Email || '',
-    //   Contact: x && x.isActive ? 'Yes' : 'No',
-    //   Email: x?.roleName || ''
+    //   "PO No.": x?.imeM_EmpId || '',
+    //   "PO Date": x?.imeM_Name || '',
+    //   Department: x?.imeM_Username || '',
+    //   Category: x?.imeM_Email || '',
+    //   "Vendor Name": x && x.isActive ? 'Yes' : 'No',
+    //   Amount: x?.roleName || '',
+    //   Status: x?.roleName || '',
+    //   Approver: x?.roleName || '',
+     //   "Update Date": x?.roleName || '',
     // }));
 
-    // const headers = ['Cust. No.','Name', 'Address', 'City','Contact', 'Email'];
+    // const headers = ['PO No.','PO Date', 'Department', 'Category','Vendor Name', 'Amount','Status','Approver','Update Date'];
     // this.appService.exportAsExcelFile(exportData,'Customer Status',headers);
   }
 
