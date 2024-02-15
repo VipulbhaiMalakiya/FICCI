8eecc53595d0e8d7497df24ac00c5c9c192db4b2
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-15 08:49:58 +0530

    update

--- src/app/Modules/invoice/component/new-purchase-invoice/new-purchase-invoice.component.ts
+++ src/app/Modules/invoice/component/new-purchase-invoice/new-purchase-invoice.component.ts
@@ -5,37 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './new-purchase-invoice.component.html',
   styleUrls: ['./new-purchase-invoice.component.css']
 })
-export class NewPurchaseInvoiceComponent implements OnInit{
-  items: any[] = [
-    { name: '', unit: '', discount: '', rate: '', amount: 0 }
-  ];
-  totalAmount: number = 0;
-  totalAmountInWords: string = '';
+export class NewPurchaseInvoiceComponent implements OnInit {
+
 
   ngOnInit(): void {
-    this.calculateTotal();
 
   }
-  calculateTotal(): void {
-    this.totalAmount = this.items.reduce((total, item) => total + parseFloat(item.amount || '0'), 0);
-  }
 
+  //
+  items: any[] = [
+    { name: 'I phone 15 Pro Max', unit: '100', discount: '10', rate: '150000', amount: 0 }
+  ];
   addRow(): void {
     this.items.push({ name: '', unit: '', discount: '', rate: '', amount: 0 });
-    this.calculateTotal(); // Recalculate total when adding a row
   }
-
   removeRow(index: number): void {
     this.items.splice(index, 1);
-    this.calculateTotal(); // Recalculate total when removing a row
   }
-  editRow(data:any):void{
-
+  calculateAmount(item: any): number {
+    const unit = parseFloat(item.unit) || 0;
+    const discount = parseFloat(item.discount) || 0;
+    const rate = parseFloat(item.rate) || 0;
+    const amount = unit * rate * (1 - discount / 100);
+    return isNaN(amount) ? 0 : amount;
   }
-
-  submitRow(data:any){
-    
+  calculateTotalAmount(): number {
+    let total = 0;
+    for (let item of this.items) {
+      total += this.calculateAmount(item);
+    }
+    return total;
   }
- 
- 
 }
