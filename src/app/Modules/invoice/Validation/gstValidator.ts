70c09a1463a8a4be22de28e7025c21764f9f2139
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-19 12:43:45 +0530

    GST Number validation

--- src/app/Modules/invoice/Validation/gstValidator.ts
+++ src/app/Modules/invoice/Validation/gstValidator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 export function gstValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
-        const gstRegex = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[0-9A-Z]{2}$/;
+        const gstRegex =/^\d{2}[A-Z]{5}\d{4}[A-Z]{1}\d[Z]{1}[A-Z\d]{1}$/;
         const gstNumber = control.value;
 
         if (!gstNumber || !gstRegex.test(gstNumber)) {
