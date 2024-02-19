0dda9fa80558625aac348b011c95013c716cd9fa
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-19 20:24:19 +0530

    update

--- src/app/layouts/login/login.component.ts
+++ src/app/layouts/login/login.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+    dataForm!: FormGroup;
+    hidePassword: boolean = true;
 
+
+    constructor(private toastr: ToastrService, private fb: FormBuilder,) {
+        this.initializeForm();
+    }
+
+    private initializeForm(): void {
+        this.dataForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required]
+        })
+    }
+
+    togglePasswordVisibility() {
+        this.hidePassword = !this.hidePassword;
+      }
+
+    onSubmit() {
+        if (this.dataForm.valid) {
+            // Here you can implement your login logic, such as sending the form data to a server
+            console.log('Form submitted!', this.dataForm.value);
+        } else {
+            this.markFormControlsAsTouched();
+        }
+    }
+
+    markFormControlsAsTouched(): void {
+        ['email','password'].forEach(controlName => {
+                this.dataForm.controls[controlName].markAsTouched();
+            });
+    }
+
+    shouldShowError(controlName: string, errorName: string): boolean {
+        return this.dataForm.controls[controlName].touched && this.dataForm.controls[controlName].hasError(errorName);
+    }
 }
