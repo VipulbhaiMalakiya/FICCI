00af022a1c721d370939aa6dca3d69befb764c5b
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-21 10:45:43 +0530

    update dashboard

---
+++ src/app/Modules/landing-page/service/customer.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+    constructor(private http: HttpClient) { }
+  private getCustomerStatusNewURL = `${environment.apiURL}Customer?email=`;
+  private retry: any = retry(1); // Retry the request up to 2 times in case of failure
+
+
+  getCustomerStatusNew(): Observable<any[]> {
+    const url = `${this.getCustomerStatusNewURL}${localStorage.getItem('userEmail') ?? ''}`;
+    return this.http.get<any[]>(url).pipe(this.retry);
+}
+}
