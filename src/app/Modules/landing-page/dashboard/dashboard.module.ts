165004fe6bc8856c237ca43954ae2e3cf4ea96bc
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-21 12:30:24 +0530

    update customer dashboard

--- src/app/Modules/landing-page/dashboard/dashboard.module.ts
+++ src/app/Modules/landing-page/dashboard/dashboard.module.ts
@@ -6,19 +6,25 @@ import { DashboardComponent } from './dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SearchFilterPipe } from '../pipe/searchFilter';
 import { ApprovalSearchFilterPipe } from '../pipe/approvalSearch';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    SearchFilterPipe,
-    ApprovalSearchFilterPipe
-  ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
-  ]
+    declarations: [
+        DashboardComponent,
+        SearchFilterPipe,
+        ApprovalSearchFilterPipe
+    ],
+    imports: [
+        CommonModule,
+        DashboardRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        NgSelectModule,
+        SharedModule,
+        NgxPaginationModule
+    ]
 })
 export class DashboardModule { }
