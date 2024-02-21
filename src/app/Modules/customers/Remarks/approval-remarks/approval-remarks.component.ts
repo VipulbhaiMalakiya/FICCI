49c021d7336f0760804d9967649b681d1c40c5f4
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-21 07:23:12 +0530

    update

--- src/app/Modules/customers/Remarks/approval-remarks/approval-remarks.component.ts
+++ src/app/Modules/customers/Remarks/approval-remarks/approval-remarks.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-approval-remarks',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./approval-remarks.component.css']
 })
 export class ApprovalRemarksComponent {
+    customerId?: number;
+    data: any;
+
+    constructor(private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.customerId = +params['id'];
+        });
+        this.data = history.state.data;
+        console.log(this.data);
+
+    }
 
 }
