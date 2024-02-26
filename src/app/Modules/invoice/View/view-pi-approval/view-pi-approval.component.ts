075e045cd1353ed88ffcae9eeba01d94d391f618
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-26 13:43:07 +0530

    update

--- src/app/Modules/invoice/View/view-pi-approval/view-pi-approval.component.ts
+++ src/app/Modules/invoice/View/view-pi-approval/view-pi-approval.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-view-pi-approval',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-pi-approval.component.css']
 })
 export class ViewPiApprovalComponent {
+    headerId?: number;
+    data: any;
+    FilePath:any;
+    constructor(private route: ActivatedRoute) { }
 
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.headerId = +params['id'];
+        });
+        this.data = history.state.data;
+        this.FilePath = `${environment.fileURL}${ this.data.impiHeaderAttachment}`;
+        console.log( this.FilePath);
+
+    }
 }
