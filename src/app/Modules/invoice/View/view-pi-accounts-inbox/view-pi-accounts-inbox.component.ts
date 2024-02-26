cfd32d54ed6e4b3b69eaf96845674733bc45e2c0
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-26 14:54:14 +0530

    update

--- src/app/Modules/invoice/View/view-pi-accounts-inbox/view-pi-accounts-inbox.component.ts
+++ src/app/Modules/invoice/View/view-pi-accounts-inbox/view-pi-accounts-inbox.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from 'src/environments/environment';
+import { publicVariable } from '../../Export/invoce';
+import { InvoicesService } from '../../service/invoices.service';
 
 @Component({
   selector: 'app-view-pi-accounts-inbox',
@@ -6,5 +13,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-pi-accounts-inbox.component.css']
 })
 export class ViewPiAccountsInboxComponent {
+    headerId?: number;
+    data: any;
+    FilePath:any;
+    publicVariable = new publicVariable();
 
+    constructor(private fb: FormBuilder,
+        private modalService: NgbModal,
+        private toastr: ToastrService,
+        private router: Router,
+        private cd: ChangeDetectorRef,
+        private API: InvoicesService,
+        private route: ActivatedRoute
+    ) {
+        this.initializeForm();
+    }
+
+    private initializeForm(): void {
+        this.publicVariable.dataForm = this.fb.group({
+            remarks: ['', [Validators.required]],
+        })
+    }
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.headerId = +params['id'];
+        });
+        this.data = history.state.data;
+        this.FilePath = `${environment.fileURL}${ this.data.impiHeaderAttachment}`;
+        console.log( this.FilePath);
+
+    }
+
+    onSubmit(action: boolean) {
+        if (this.publicVariable.dataForm.valid) {
+            const newData = this.publicVariable.dataForm.value;
+
+            const newConfig: any = {
+                headerId: this.data.headerId,
+                isApproved: action,
+                loginId: this.publicVariable.storedEmail,
+                statusId: this.data.headerStatusId,
+                remarks: newData.remarks,
+            }
+            this.publicVariable.isProcess = true;
+            this.publicVariable.Subscription.add(
+                this.API.isApproverRemarks(newConfig).subscribe({
+                    next: (res: any) => {
+                        if (res.status === true) {
+                            this.toastr.success(res.message, 'Success');
+                            this.router.navigate(['customer/approval']);
+                            this.publicVariable.dataForm.reset();
+                        } else {
+                            this.toastr.error(res.message, 'Error');
+                        }
+                    },
+                    error: (error: any) => {
+                        this.publicVariable.isProcess = false;
+                        this.toastr.error(error.error.message || 'An error occurred. Please try again later.', 'Error');
+                    },
+                    complete: () => {
+                        this.publicVariable.isProcess = false;
+                    }
+                })
+            );
+
+        } else {
+            this.markFormControlsAsTouched();
+
+        }
+
+    }
+
+    markFormControlsAsTouched(): void {
+        ['remarks'].forEach(controlName => {
+            this.publicVariable.dataForm.controls[controlName].markAsTouched();
+        });
+    }
+
+    shouldShowError(controlName: string, errorName: string): boolean {
+        return this.publicVariable.dataForm.controls[controlName].touched && this.publicVariable.dataForm.controls[controlName].hasError(errorName);
+    }
 }
+
