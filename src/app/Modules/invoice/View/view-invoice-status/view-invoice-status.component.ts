eea0af0adbaa36ad166fe1413e019991aa60be91
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-27 16:20:50 +0530

    update

--- src/app/Modules/invoice/View/view-invoice-status/view-invoice-status.component.ts
+++ src/app/Modules/invoice/View/view-invoice-status/view-invoice-status.component.ts
@@ -20,6 +20,7 @@ export class ViewInvoiceStatusComponent {
             this.headerId = +params['id'];
         });
         this.data = history.state.data;
+
         this.FilePath = `${environment.fileURL}${ this.data.impiHeaderAttachment}`;
         this.loadStateList();
     }
@@ -29,7 +30,7 @@ export class ViewInvoiceStatusComponent {
             const subscription = this.CAPI.getStateList().subscribe({
                 next: (response: any) => {
                     this.publicVariable.stateList = response.data;
-                    this.loadCityList();
+                    this.handleLoadingError()
                 },
                 error: (error) => {
                     console.error('Error loading project list:', error);
@@ -44,37 +45,12 @@ export class ViewInvoiceStatusComponent {
         }
     }
 
-    loadCityList() {
-        try {
-            const subscription = this.CAPI.getCityList().subscribe({
-                next: (response: any) => {
-                    this.publicVariable.cityList = response.data;
-                    this.handleLoadingError();
-                },
-                error: (error) => {
-                    console.error('Error loading city list:', error);
-                    console.error('Failed to load city list. Please try again later.');
-                    this.handleLoadingError();
-                },
-            });
-
-            this.publicVariable.Subscription.add(subscription);
-        } catch (error) {
-            console.error('Error loading city list:', error);
-            console.error('An unexpected error occurred. Please try again later.');
-            this.handleLoadingError();
-        }
-    }
 
     getStateNameById(stateId:string) {
         const state = this.publicVariable.stateList.find(state => state.stateCode === stateId);
         return state ? state.stateName : null;
     }
 
-    getCityNameById(cityId:any) {
-        const city = this.publicVariable.cityList.find(city => city.cityCode === cityId);
-        return city ? city.cityName : null;
-    }
 
     handleLoadingError() {
         this.publicVariable.isProcess = false; // Set status to false on error
