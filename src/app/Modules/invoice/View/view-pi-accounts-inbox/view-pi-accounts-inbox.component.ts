43435306c7fa72b944e4680000e98e0762d7592d
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-27 08:48:35 +0530

    update

--- src/app/Modules/invoice/View/view-pi-accounts-inbox/view-pi-accounts-inbox.component.ts
+++ src/app/Modules/invoice/View/view-pi-accounts-inbox/view-pi-accounts-inbox.component.ts
@@ -4,18 +4,18 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { environment } from 'src/environments/environment';
-import { publicVariable } from '../../Export/invoce';
+import { CustomersService, publicVariable } from '../../Export/invoce';
 import { InvoicesService } from '../../service/invoices.service';
 
 @Component({
-  selector: 'app-view-pi-accounts-inbox',
-  templateUrl: './view-pi-accounts-inbox.component.html',
-  styleUrls: ['./view-pi-accounts-inbox.component.css']
+    selector: 'app-view-pi-accounts-inbox',
+    templateUrl: './view-pi-accounts-inbox.component.html',
+    styleUrls: ['./view-pi-accounts-inbox.component.css']
 })
 export class ViewPiAccountsInboxComponent {
     headerId?: number;
     data: any;
-    FilePath:any;
+    FilePath: any;
     publicVariable = new publicVariable();
 
     constructor(private fb: FormBuilder,
@@ -24,14 +24,15 @@ export class ViewPiAccountsInboxComponent {
         private router: Router,
         private cd: ChangeDetectorRef,
         private API: InvoicesService,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private CAPI: CustomersService,
     ) {
         this.initializeForm();
     }
 
     private initializeForm(): void {
         this.publicVariable.dataForm = this.fb.group({
-            remarks: ['', [Validators.required]],
+            remarks: [''],
         })
     }
 
@@ -40,9 +41,64 @@ export class ViewPiAccountsInboxComponent {
             this.headerId = +params['id'];
         });
         this.data = history.state.data;
-        this.FilePath = `${environment.fileURL}${ this.data.impiHeaderAttachment}`;
-        console.log( this.FilePath);
+        this.FilePath = `${environment.fileURL}${this.data.impiHeaderAttachment}`;
+        this.loadStateList();
 
+    }
+    loadStateList() {
+        try {
+            const subscription = this.CAPI.getStateList().subscribe({
+                next: (response: any) => {
+                    this.publicVariable.stateList = response.data;
+                    this.loadCityList();
+                },
+                error: (error) => {
+                    console.error('Error loading project list:', error);
+                    this.handleLoadingError()
+                },
+            });
+
+            this.publicVariable.Subscription.add(subscription);
+        } catch (error) {
+            console.error('Error loading project list:', error);
+            this.handleLoadingError()
+        }
+    }
+
+    loadCityList() {
+        try {
+            const subscription = this.CAPI.getCityList().subscribe({
+                next: (response: any) => {
+                    this.publicVariable.cityList = response.data;
+                    this.handleLoadingError();
+                },
+                error: (error) => {
+                    console.error('Error loading city list:', error);
+                    console.error('Failed to load city list. Please try again later.');
+                    this.handleLoadingError();
+                },
+            });
+
+            this.publicVariable.Subscription.add(subscription);
+        } catch (error) {
+            console.error('Error loading city list:', error);
+            console.error('An unexpected error occurred. Please try again later.');
+            this.handleLoadingError();
+        }
+    }
+
+    getStateNameById(stateId: string) {
+        const state = this.publicVariable.stateList.find(state => state.stateCode === stateId);
+        return state ? state.stateName : null;
+    }
+
+    getCityNameById(cityId: any) {
+        const city = this.publicVariable.cityList.find(city => city.cityCode === cityId);
+        return city ? city.cityName : null;
+    }
+
+    handleLoadingError() {
+        this.publicVariable.isProcess = false; // Set status to false on error
     }
 
     onSubmit(action: boolean) {
