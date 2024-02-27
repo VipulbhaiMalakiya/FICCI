c166418ffa084865f7d7a7f7906998853918c991
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-27 10:35:07 +0530

    is update

--- src/app/Modules/invoice/View/view-pi-accounts-inbox/view-pi-accounts-inbox.component.ts
+++ src/app/Modules/invoice/View/view-pi-accounts-inbox/view-pi-accounts-inbox.component.ts
@@ -20,50 +20,44 @@ export class ViewPiAccountsInboxComponent implements OnInit, OnDestroy {
     publicVariable = new publicVariable();
 
     editorConfig: AngularEditorConfig = {
+
         editable: true,
-          spellcheck: true,
-          height: 'auto',
-          minHeight: '0',
-          maxHeight: 'auto',
-          width: 'auto',
-          minWidth: '0',
-          translate: 'yes',
-          enableToolbar: true,
-          showToolbar: true,
-          placeholder: 'Enter text here...',
-          defaultParagraphSeparator: '',
-          defaultFontName: '',
-          defaultFontSize: '',
-          fonts: [
-            {class: 'arial', name: 'Arial'},
-            {class: 'times-new-roman', name: 'Times New Roman'},
-            {class: 'calibri', name: 'Calibri'},
-            {class: 'comic-sans-ms', name: 'Comic Sans MS'}
-          ],
-          customClasses: [
-          {
-            name: 'quote',
-            class: 'quote',
-          },
-          {
-            name: 'redText',
-            class: 'redText'
-          },
-          {
-            name: 'titleText',
-            class: 'titleText',
-            tag: 'h1',
-          },
+        spellcheck: true,
+        height: "15rem",
+        minHeight: "5rem",
+        placeholder: "Enter text here...",
+        translate: "no",
+        defaultParagraphSeparator: "p",
+        defaultFontName: "Arial",
+        sanitize: false,
+
+        maxHeight: 'auto',
+        width: 'auto',
+        minWidth: '0',
+        enableToolbar: true,
+        showToolbar: true,
+        fonts: [
+            { class: 'arial', name: 'Arial' },
+            { class: 'times-new-roman', name: 'Times New Roman' },
+            { class: 'calibri', name: 'Calibri' },
+            { class: 'comic-sans-ms', name: 'Comic Sans MS' }
         ],
-        uploadUrl: 'v1/image',
-        // upload: (file: File) => { ... }
-        // uploadWithCredentials: false,
-        // sanitize: true,
-        // toolbarPosition: 'top',
-        // toolbarHiddenButtons: [
-        //   ['bold', 'italic'],
-        //   ['fontSize']
-        // ]
+        customClasses: [
+            {
+                name: 'quote',
+                class: 'quote',
+            },
+            {
+                name: 'redText',
+                class: 'redText'
+            },
+            {
+                name: 'titleText',
+                class: 'titleText',
+                tag: 'h1',
+            },
+        ],
+
     };
 
 
@@ -97,7 +91,7 @@ export class ViewPiAccountsInboxComponent implements OnInit, OnDestroy {
     }
 
     ngOnDestroy(): void {
-      }
+    }
     loadStateList() {
         try {
             const subscription = this.CAPI.getStateList().subscribe({
