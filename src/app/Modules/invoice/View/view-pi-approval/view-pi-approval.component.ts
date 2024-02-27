724c405d2ba8b4e9204622e6e6449bbbad8f9e28
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-27 08:44:50 +0530

    update

--- src/app/Modules/invoice/View/view-pi-approval/view-pi-approval.component.ts
+++ src/app/Modules/invoice/View/view-pi-approval/view-pi-approval.component.ts
@@ -20,14 +20,15 @@ export class ViewPiApprovalComponent {
         private router: Router,
         private cd: ChangeDetectorRef,
         private API: InvoicesService,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private CAPI: CustomersService
     ) {
         this.initializeForm();
     }
 
     private initializeForm(): void {
         this.publicVariable.dataForm = this.fb.group({
-            remarks: ['', [Validators.required]],
+            remarks: [''],
         })
     }
 
@@ -37,8 +38,64 @@ export class ViewPiApprovalComponent {
         });
         this.data = history.state.data;
         this.FilePath = `${environment.fileURL}${ this.data.impiHeaderAttachment}`;
-        console.log( this.FilePath);
+        this.loadStateList();
+    }
 
+
+    loadStateList() {
+        try {
+            const subscription = this.CAPI.getStateList().subscribe({
+                next: (response: any) => {
+                    this.publicVariable.stateList = response.data;
+                    this.loadCityList();
+                },
+                error: (error) => {
+                    console.error('Error loading project list:', error);
+                    this.handleLoadingError()
+                },
+            });
+
+            this.publicVariable.Subscription.add(subscription);
+        } catch (error) {
+            console.error('Error loading project list:', error);
+            this.handleLoadingError()
+        }
+    }
+
+    loadCityList() {
+        try {
+            const subscription = this.CAPI.getCityList().subscribe({
+                next: (response: any) => {
+                    this.publicVariable.cityList = response.data;
+                    this.handleLoadingError();
+                },
+                error: (error) => {
+                    console.error('Error loading city list:', error);
+                    console.error('Failed to load city list. Please try again later.');
+                    this.handleLoadingError();
+                },
+            });
+
+            this.publicVariable.Subscription.add(subscription);
+        } catch (error) {
+            console.error('Error loading city list:', error);
+            console.error('An unexpected error occurred. Please try again later.');
+            this.handleLoadingError();
+        }
+    }
+
+    getStateNameById(stateId:string) {
+        const state = this.publicVariable.stateList.find(state => state.stateCode === stateId);
+        return state ? state.stateName : null;
+    }
+
+    getCityNameById(cityId:any) {
+        const city = this.publicVariable.cityList.find(city => city.cityCode === cityId);
+        return city ? city.cityName : null;
+    }
+
+    handleLoadingError() {
+        this.publicVariable.isProcess = false; // Set status to false on error
     }
 
     onSubmit(action: boolean) {
