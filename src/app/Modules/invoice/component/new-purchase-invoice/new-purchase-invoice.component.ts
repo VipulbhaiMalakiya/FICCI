f4e2847477cd79425a2ab6a0f1663eefb0801aae
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-29 12:37:06 +0530

    update

--- src/app/Modules/invoice/component/new-purchase-invoice/new-purchase-invoice.component.ts
+++ src/app/Modules/invoice/component/new-purchase-invoice/new-purchase-invoice.component.ts
@@ -81,7 +81,7 @@ export class NewPurchaseInvoiceComponent implements OnInit {
         return concatenatedString.includes(term.toLowerCase());
     }
     customerSearchFn(term: string, item: any) {
-        const concatenatedString = `${item.custName} ${item.custName2} ${item.panNo} ${item.gstregistrationNo}`.toLowerCase();
+        const concatenatedString = `${item.custNo} ${item.custName} ${item.custName2} ${item.panNo} ${item.gstregistrationNo}`.toLowerCase();
         return concatenatedString.includes(term.toLowerCase());
     }
 
