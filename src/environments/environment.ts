ef3e858f3b825a582219531d5cd1cfb16b8e25cd
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-29 08:24:09 +0530

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     //Development Local
-    // apiURL: "https://localhost:7038/api/",
-    // fileURL: "https://localhost:7038/",
+    apiURL: "https://localhost:7038/api/",
+    fileURL: "https://localhost:7038/",
 
     //Development Server
-    apiURL: "http://172.16.19.154:1220/api/",
-    fileURL: "http://172.16.19.154:1220/",
+    // apiURL: "http://172.16.19.154:1220/api/",
+    // fileURL: "http://172.16.19.154:1220/",
 
   };
 
