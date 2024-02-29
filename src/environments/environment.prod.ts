bdda70bcf031d1cc042ee6c43e5cbac0acf48a2f
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-29 07:43:47 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
-};
+    production: true,
+    //Development Server
+    apiURL: "http://172.16.19.154:1220/api/",
+    fileURL: "http://172.16.19.154:1220/",
+
+  };
