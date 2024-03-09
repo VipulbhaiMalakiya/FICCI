754bcf520b23e5426da0477502fa50e2e1737632
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-03-09 22:52:38 +0530

    update

--- src/app/Modules/invoice/View/posted-tax-invoice/posted-tax-invoice.component.ts
+++ src/app/Modules/invoice/View/posted-tax-invoice/posted-tax-invoice.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CustomersService, publicVariable } from '../../Export/invoce';
+import { InvoicesService } from '../../service/invoices.service';
 
 @Component({
   selector: 'app-posted-tax-invoice',
@@ -6,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./posted-tax-invoice.component.css']
 })
 export class PostedTaxInvoiceComponent {
+    invoice_no?: number;
+    data: any;
+    publicVariable = new publicVariable();
+
+
+    constructor(private route: ActivatedRoute, private CAPI: CustomersService,
+        private API: InvoicesService,
+        private toastr: ToastrService,
+        private router: Router,
+        private fb: FormBuilder,
+    ) {
+
+    }
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.invoice_no = +params['id'];
+        });
+        this.data = history.state.data;
+        console.log(this.data);
+
+
+
+
+    }
 
 }
