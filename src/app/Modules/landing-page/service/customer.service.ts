60cfb7d0443328800420b62fbb1d3cd7c8fa2323
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-03-13 07:33:33 +0530

    update

--- src/app/Modules/landing-page/service/customer.service.ts
+++ src/app/Modules/landing-page/service/customer.service.ts
@@ -11,11 +11,10 @@ export class CustomerService {
 
     constructor(private http: HttpClient) { }
     private getCustomerStatusNewURL = `${environment.apiURL}Customer?email=`;
-    private retry: any = retry(1); // Retry the request up to 2 times in case of failure
 
 
     getCustomerStatusNew(): Observable<any[]> {
         const url = `${this.getCustomerStatusNewURL}${localStorage.getItem('userEmail') ?? ''}`;
-        return this.http.get<any[]>(url).pipe(this.retry);
+        return this.http.get<any[]>(url);
     }
 }
