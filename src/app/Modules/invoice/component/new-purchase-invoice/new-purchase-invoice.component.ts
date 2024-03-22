112bb46c399a7d2d6fadde0ce861fbf4e89b90a9
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-03-22 09:18:30 +0530

    update

--- src/app/Modules/invoice/component/new-purchase-invoice/new-purchase-invoice.component.ts
+++ src/app/Modules/invoice/component/new-purchase-invoice/new-purchase-invoice.component.ts
@@ -910,24 +910,14 @@ export class NewPurchaseInvoiceComponent implements OnInit {
 
                                     if (Action == "Calculate") {
                                         this.patchFormData(this.data);
-                                        // alert('Tax Calculated Successfully !!');
-
                                         this.toastr.success('Tax Calculated Successfully !!', 'Success');
                                     }
                                     else {
                                         this.toastr.success(res.message, 'Success');
                                         this.router.navigate(['invoice/status']);
-                                        // alert('Tax Calculated Successfully !!');
-
                                         this.publicVariable.dataForm.reset();
                                     }
 
-                                    //  this.toastr.success(res.message, 'Success');
-                                    //this.patchFormData( this.data)
-
-
-                                    //this.router.navigate(['invoice/status']);
-                                    // this.publicVariable.dataForm.reset();
                                 } else {
                                     this.toastr.error(res.message, 'Error');
                                     this.publicVariable.isProcess = false;
