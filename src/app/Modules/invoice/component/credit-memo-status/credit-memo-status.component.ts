d4956aac439146590fa7c85a6e308997a5ff29f3
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-03-26 16:35:41 +0530

    update

--- src/app/Modules/invoice/component/credit-memo-status/credit-memo-status.component.ts
+++ src/app/Modules/invoice/component/credit-memo-status/credit-memo-status.component.ts
@@ -1,10 +1,185 @@
-import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { publicVariable, AppService, CustomersService, ConfirmationDialogModalComponent } from '../../Export/invoce';
+import { invoiceStatusModule } from '../../interface/invoice';
+import { InvoicesService } from '../../service/invoices.service';
 
 @Component({
   selector: 'app-credit-memo-status',
   templateUrl: './credit-memo-status.component.html',
   styleUrls: ['./credit-memo-status.component.css']
 })
-export class CreditMemoStatusComponent {
+export class CreditMemoStatusComponent implements OnInit {
+    publicVariable = new publicVariable();
 
+
+    constructor(private appService: AppService,
+        private modalService: NgbModal,
+        private router: Router,
+        private toastr: ToastrService,
+        private API: InvoicesService,
+        private cdr: ChangeDetectorRef,
+        private CAPI: CustomersService
+
+    ) {
+
+    }
+
+    ngOnInit(): void {
+        this.loadPurchaseInvoiceList();
+        this.loadStateList();
+    }
+    loadPurchaseInvoiceList(): void {
+        try {
+            const subscription = this.API.getPurchaseInvoice_New().subscribe({
+                next: (response: any) => {
+                    if (response.data && Array.isArray(response.data)) {
+                        this.publicVariable.invoiceStatuslistData = response.data;
+                        this.publicVariable.count = response.data.length;
+                    } else {
+                        // Handle case where response data is null or not an array
+                        this.publicVariable.invoiceStatuslistData = [];
+                        this.publicVariable.count = 0;
+                        console.warn('Response data is null or not an array:', response.data);
+                    }
+                },
+                error: (error) => {
+                    console.error('Error loading project list:', error);
+                }
+            });
+
+            this.publicVariable.Subscription.add(subscription);
+        } catch (error) {
+            console.error('Error loading project list:', error);
+        }
+    }
+
+    toTitleCase(str: string): string {
+        return str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    }
+
+    onView(data: invoiceStatusModule): void {
+        if (data.headerId) {
+            this.router.navigate(['invoice/status/view', data.headerId], { state: { data: data } });
+        } else {
+            console.error('ID is undefined or null');
+        }
+    }
+
+    onDelete(id: any) {
+        const modalRef = this.modalService.open(ConfirmationDialogModalComponent, { size: "sm", centered: true, backdrop: "static" });
+        var componentInstance = modalRef.componentInstance as ConfirmationDialogModalComponent;
+        componentInstance.message = "Do you really want to delete these records? This process cannot be undone ?";
+        modalRef.result.then((canDelete: boolean) => {
+            if (canDelete) {
+                this.publicVariable.isProcess = true;
+                this.API.delete(id).subscribe({
+                    next: (res: any) => {
+                        this.toastr.success(res.message, 'Success');
+                        this.publicVariable.isProcess = false;
+                        this.cdr.detectChanges();
+                        this.loadPurchaseInvoiceList();
+
+                    },
+                    error: (error) => {
+                        this.publicVariable.isProcess = false;
+                        this.toastr.error(error.error.message, 'Error');
+                    }
+                });
+
+            }
+        }).catch(() => { });
+
+    }
+
+    onEdit(data: invoiceStatusModule): void {
+        if (data.headerId) {
+            this.router.navigate(['invoice/status/edit', data.headerId], { state: { data: data } });
+        } else {
+            console.error('ID is undefined or null');
+        }
+    }
+
+    loadStateList() {
+        try {
+            const subscription = this.CAPI.getStateList().subscribe({
+                next: (response: any) => {
+                    this.publicVariable.stateList = response.data;
+                    this.handleLoadingError();
+                },
+                error: (error) => {
+                    console.error('Error loading project list:', error);
+                    this.handleLoadingError()
+                },
+            });
+
+            this.publicVariable.Subscription.add(subscription);
+        } catch (error) {
+            console.error('Error loading project list:', error);
+            this.handleLoadingError()
+        }
+    }
+
+    getStateNameById(stateId:string) {
+        const state = this.publicVariable.stateList.find(state => state.stateCode === stateId);
+        return state ? state.stateName : null;
+    }
+
+    handleLoadingError() {
+        this.publicVariable.isProcess = false; // Set status to false on error
+    }
+
+    onDownload() {
+        const exportData = this.publicVariable.invoiceStatuslistData.map((x) => ({
+            "PO No.": x?.impiHeaderProjectCode || '',
+            'Project' :x?.impiHeaderProjectName ? this.toTitleCase(x.impiHeaderProjectName) : '',
+            Department: x?.impiHeaderProjectDepartmentName ? this.toTitleCase(x.impiHeaderProjectDepartmentName) : '',
+            Divison: x?.impiHeaderProjectDivisionName ? this.toTitleCase(x.impiHeaderProjectDivisionName) : '',
+            Category: x?.impiHeaderInvoiceType ? this.toTitleCase(x.impiHeaderInvoiceType) : '',
+            "PAN No": x?.impiHeaderPanNo || '',
+            "State": this.getStateNameById(x?.impiHeaderCustomerState),
+            "City": x?.impiHeaderCustomerCity,
+            "Pincode": x?.impiHeaderCustomerPinCode || '',
+            "Vendor Name": x && x.impiHeaderCustomerName ? this.toTitleCase(x.impiHeaderCustomerName) : '',
+            "Address": x?.impiHeaderCustomerAddress,
+            'Customer  GST Number': x?.impiHeaderCustomerGstNo || '',
+            'Contact Person': x?.impiHeaderCustomerContactPerson || '',
+            'Phone No': x?.impiHeaderCustomerPhoneNo || '',
+            "Email ID": x?.impiHeaderCustomerEmailId || '',
+            Amount: x?.impiHeaderTotalInvoiceAmount != null ? (x.impiHeaderTotalInvoiceAmount).toLocaleString('en-IN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) : '',
+            'Payment Terms': x?.impiHeaderPaymentTerms || '',
+            'impiHeaderRemarks': x?.impiHeaderRemarks || '',
+            'Tl Approver': x?.impiHeaderTlApprover ? this.toTitleCase(x.impiHeaderTlApprover) : '',
+            'Cl Approver': x?.impiHeaderClusterApprover ? this.toTitleCase(x.impiHeaderClusterApprover) : '',
+            'Finance Approver': x?.impiHeaderFinanceApprover ? this.toTitleCase(x.impiHeaderFinanceApprover) : '',
+            'Accounts Approver': x?.accountApprover ? this.toTitleCase(x.accountApprover) : '',
+            'Created On':x?.impiHeaderSubmittedDate ? formatDate(x.impiHeaderSubmittedDate, 'medium', 'en-IN', 'IST') : '',
+            'Created By': x?.impiHeaderCreatedBy ? this.toTitleCase(x.impiHeaderCreatedBy) : '',
+            "Update Date": x?.impiHeaderModifiedDate ? formatDate(x.impiHeaderModifiedDate, 'medium', 'en-IN', 'IST') : '',
+            'Status':x?.headerStatus ? this.toTitleCase(x?.headerStatus) : ''
+        }));
+
+        const headers = [
+            'PO No.','Project', 'Department', 'Divison', 'Category',
+            'Vendor Name', 'Address', 'State', 'City', 'Pincode',
+            'Phone No', "Email ID", 'Contact Person', 'Customer  GST Number', 'PAN No', 'Amount', 'Payment Terms',
+            'impiHeaderRemarks', 'Tl Approver', 'Cl Approver', 'Finance Approver', 'Accounts Approver','Created On', 'Created By','Update Date',
+            'Status'
+        ];
+        this.appService.exportAsExcelFile(exportData, 'PI Invoice Status', headers);
+    }
+
+    onTableDataChange(event: any) {
+        this.publicVariable.page = event;
+        this.publicVariable.invoiceStatuslistData
+    }
+    onTableSizeChange(event: any): void {
+        this.publicVariable.tableSize = event.target.value;
+        this.publicVariable.page = 1;
+        this.publicVariable.invoiceStatuslistData
+
+    }
 }
