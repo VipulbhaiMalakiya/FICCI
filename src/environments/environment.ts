e26c414dab10121a23e2e3bb35d4a94ad81df0df
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-04-09 10:54:53 +0530

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     //Development Local
-    // apiURL: "https://localhost:7038/api/",
-    // fileURL: "https://localhost:7038/",
+    apiURL: "https://localhost:7038/api/",
+    fileURL: "https://localhost:7038/",
 
     //Production Server
-    apiURL: "http://siportal.myassociates.org:9001/api/",
-    fileURL: "http://siportal.myassociates.org:9001/",
+    // apiURL: "http://siportal.myassociates.org:9001/api/",
+    // fileURL: "http://siportal.myassociates.org:9001/",
 
 };
 
