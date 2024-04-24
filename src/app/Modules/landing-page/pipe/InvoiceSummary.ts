1974398690732b2d9973414719c0c6bb56f680ef
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-04-24 15:23:28 +0530

    Update

--- src/app/Modules/landing-page/pipe/InvoiceSummary.ts
+++ src/app/Modules/landing-page/pipe/InvoiceSummary.ts
@@ -10,21 +10,28 @@ export class invoiceFilterPipe implements PipeTransform {
         }
         searchText = searchText.toLowerCase();
         return items.filter(item => {
-            return item.headerRecordID.toLowerCase().includes(searchText) ||
-             item.customerStatus.toLowerCase().includes(searchText)
-             || item.headerPiNo.toLowerCase().includes(searchText)
-             || item.impiHeaderProjectCode.toLowerCase().includes(searchText)
-             || item.impiHeaderProjectName.toLowerCase().includes(searchText)
-             || item.impiHeaderProjectDepartmentName.projectCode().includes(searchText)
-             || item.impiHeaderProjectDivisionName.toLowerCase().includes(searchText)
-             || item.impiHeaderCustomerName.toLowerCase().includes(searchText)
-             || item.impiHeaderCustomerCity.toLowerCase().includes(searchText)
-             || item.impiHeaderTotalInvoiceAmount.toLowerCase().includes(searchText)
-             || item.impiHeaderTlApprover.toLowerCase().includes(searchText)
-             || item.impiHeaderSubmittedDate.toLowerCase().includes(searchText)
-             || item.impiHeaderCreatedBy.toLowerCase().includes(searchText)
-             || item.headerStatus.toLowerCase().includes(searchText)
-             ;
+            const totalInvoiceAmount = String(item.impiHeaderTotalInvoiceAmount);
+
+            return (item.impiHeaderInvoiceType && item.impiHeaderInvoiceType.toLowerCase().includes(searchText))
+                || (item.impiHeaderProjectCode && item.impiHeaderProjectCode.toLowerCase().includes(searchText))
+                || (item.impiHeaderProjectName && item.impiHeaderProjectName.toLowerCase().includes(searchText))
+                || (item.impiHeaderProjectDepartmentName && item.impiHeaderProjectDepartmentName.toLowerCase().includes(searchText))
+                || (item.impiHeaderProjectDivisionName && item.impiHeaderProjectDivisionName.toLowerCase().includes(searchText))
+                || (item.impiHeaderCustomerName && item.impiHeaderCustomerName.toLowerCase().includes(searchText))
+                || (item.impiHeaderCustomerCity && item.impiHeaderCustomerCity.toLowerCase().includes(searchText))
+                || (totalInvoiceAmount.includes(searchText))
+                || (item.impiHeaderTlApprover && item.impiHeaderTlApprover.toLowerCase().includes(searchText))
+                || (item.impiHeaderClusterApprover && item.impiHeaderClusterApprover.toLowerCase().includes(searchText))
+                || (item.impiHeaderFinanceApprover && item.impiHeaderFinanceApprover.toLowerCase().includes(searchText))
+                || (item.accountApprover && item.accountApprover.toLowerCase().includes(searchText))
+                || (item.impiHeaderSubmittedDate && item.impiHeaderSubmittedDate.toLowerCase().includes(searchText))
+                || (item.impiHeaderCreatedBy && item.impiHeaderCreatedBy.toLowerCase().includes(searchText))
+                || (item.headerRecordID && item.headerRecordID.toLowerCase().includes(searchText))
+                || (item.sellToCustomerNo && item.sellToCustomerNo.toLowerCase().includes(searchText))
+                || (item.recordID && item.recordID.toLowerCase().includes(searchText))
+
+
+                || (item.headerStatus && item.headerStatus.toLowerCase().includes(searchText));
         });
     }
 }
