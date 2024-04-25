e97943a9fc426a3ce84799f15e5925da4990bc0d
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-04-25 09:01:46 +0530

    Update

--- src/app/Modules/invoice/Pipe/searchpi.ts
+++ src/app/Modules/invoice/Pipe/searchpi.ts
@@ -28,6 +28,10 @@ export class SearchFilterPipe2 implements PipeTransform {
                 || (item.impiHeaderCreatedBy && item.impiHeaderCreatedBy.toLowerCase().includes(searchText))
                 || (item.no && item.no.toLowerCase().includes(searchText))
                 || (item.invoice_no && item.invoice_no.toLowerCase().includes(searchText))
+                || (item.sellToCustomerNo && item.sellToCustomerNo.toLowerCase().includes(searchText))
+                || (item.sellToCustomerName && item.sellToCustomerName.toLowerCase().includes(searchText))
+                || (item.projectCode && item.projectCode.toLowerCase().includes(searchText))
+
                 || (item.recordID && item.recordID.toLowerCase().includes(searchText))
 
                 || (item.headerStatus && item.headerStatus.toLowerCase().includes(searchText));
